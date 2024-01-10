0fbff6b9b144255763a7fb451115431cdfd12ee8
choyrulh <choirul.humam@students.amikom.ac.id>
2024-01-10 13:15:23 +0700

    update

--- src/components/header/CategoryFrag.jsx
+++ src/components/header/CategoryFrag.jsx
@@ -10,6 +10,9 @@ import { HiSquares2X2 } from "react-icons/hi2";
 import { RiMacbookFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 function CategoryFrag() {
+  const handleClickWindow = () => {
+    window.scrollTo(0, 0);
+  };
   return (
     <>
       <section
@@ -17,37 +20,37 @@ function CategoryFrag() {
         h-32 items-center shadow-lg shadow-[#CCCCCC] bg-[#E0E0E0] dark:bg-[#111111] dark:shadow-[#222222]"
       >
         <ul className="h-full flex flex-row items-center gap-2 justify-evenly dark:text-[#CCCCCC]">
-          <Link to="/categories">
+          <Link to="/categories" onClick={handleClickWindow}>
             <li className="flex flex-col items-center gap-2 justify-evenly hover:text-[#444444] dark:hover:text-white ">
               <HiSquares2X2 className="h-9 w-9 bg-white dark:bg-[#222222] rounded-full p-1 shadow-inner " />
               <h2>All Categories</h2>
             </li>
           </Link>
-          <Link to="./categories/laptops">
+          <Link to="./categories/laptops" onClick={handleClickWindow}>
             <li className="flex flex-col items-center gap-2 justify-evenly hover:text-[#444444] dark:hover:text-white">
               <RiMacbookFill className="w-9 h-9 bg-white dark:bg-[#222222] rounded-full p-1 shadow-inner " />
               <h2>Laptop</h2>
             </li>
           </Link>
-          <Link to="./categories/smartphones">
+          <Link to="./categories/smartphones" onClick={handleClickWindow}>
             <li className="flex flex-col items-center gap-2 justify-evenly hover:text-[#444444] dark:hover:text-white">
               <GiSmartphone className="w-9 h-9 bg-white dark:bg-[#222222] rounded-full p-1 shadow-inner" />
               <h2>Smartphone</h2>
             </li>
           </Link>
-          <Link to="./categories/clothes">
+          <Link to="./categories/clothes" onClick={handleClickWindow}>
             <li className="flex flex-col items-center gap-2 justify-evenly hover:text-[#444444] dark:hover:text-white">
               <GiClothes className=" w-9 h-9 bg-white dark:bg-[#222222] rounded-full p-1 shadow-inner" />
               <h2>Fashion</h2>
             </li>
           </Link>
-          <Link to="./categories/groceries">
+          <Link to="./categories/groceries" onClick={handleClickWindow}>
             <li className="flex flex-col items-center gap-2 justify-evenly hover:text-[#444444] dark:hover:text-white">
               <GiShoppingBag className="w-9 h-9 bg-white dark:bg-[#222222] rounded-full p-1 shadow-inner" />
               <h2>Groceries</h2>
             </li>
           </Link>
-          <Link to="./categories/watch">
+          <Link to="./categories/watch" onClick={handleClickWindow}>
             <li className="flex flex-col items-center gap-2 justify-evenly hover:text-[#444444] dark:hover:text-white">
               <GiWatch
                 className="w-9 h-9 bg-white dark:bg-[#222222] rounded-full p-1
@@ -56,7 +59,7 @@ function CategoryFrag() {
               <h2>Watch</h2>
             </li>
           </Link>
-          <Link to="./categories/sunglasses">
+          <Link to="./categories/sunglasses" onClick={handleClickWindow}>
             <li className="flex flex-col items-center gap-2 justify-evenly hover:text-[#444444] dark:hover:text-white">
               <GiSunglasses
                 className="w-9 h-9 bg-white dark:bg-[#222222]
@@ -65,7 +68,7 @@ function CategoryFrag() {
               <h2>Sunglasses</h2>
             </li>
           </Link>
-          <Link to="./categories/furniture">
+          <Link to="./categories/furniture" onClick={handleClickWindow}>
             <li className="flex flex-col items-center gap-2 justify-evenly hover:text-[#444444] dark:hover:text-white">
               <GiSofa
                 className="w-9 h-9 bg-white dark:bg-[#222222]
--- src/components/header/SearchBar.jsx
+++ src/components/header/SearchBar.jsx
@@ -1,11 +1,13 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 
-import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
+import { HiOutlineMagnifyingGlass, HiXMark } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
   const [query, setQuery] = useState("");
+  const [queryActive, setQueryActive] = useState(false);
+  const [queryCancel, setQueryCancel] = useState(false);
   const navigate = useNavigate();
   const handleChange = (event) => {
     const value = event.target.value;
@@ -13,9 +15,19 @@ const SearchBar = () => {
 
     if (value.length > 2) {
       navigate(`/search-products/${value}`);
+      setQueryActive(true);
     } else {
       navigate("/");
+      setQueryCancel(true);
+      setQueryActive(false);
     }
+  };
+
+  const handleCancel = () => {
+    setQuery("");
+    setQueryCancel(false);
+    setQueryActive(false);
+    navigate("/");
   };
 
   return (
@@ -26,7 +38,15 @@ const SearchBar = () => {
         transition={{ duration: 0.5 }}
         className="flex flex-row items-center border p-2 rounded-md "
       >
-        <HiOutlineMagnifyingGlass className="h-5 w-5 text-gray-500 mr-2" />
+        {queryCancel && queryActive ? (
+          <HiXMark
+            onClick={handleCancel}
+            className="h-5 w-5 text-gray-500 mr-2 cursor-pointer"
+          />
+        ) : (
+          <HiOutlineMagnifyingGlass className="h-5 w-5 text-gray-500 mr-2" />
+        )}
+        {/* <HiOutlineMagnifyingGlass /> */}
         <input
           value={query}
           onChange={handleChange}
